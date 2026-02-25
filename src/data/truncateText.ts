8c1389cb357bf1ef1b9bd4be2174898efe50db4c
Ram Gollapalli <nagaramaganga@gmail.com>
2026-02-26 02:34:31 +0800

    icon size updated

---
+++ src/data/truncateText.ts
@@ -0,0 +1,5 @@
+// utils/truncateText.ts
+export const truncateText = (text: string, maxLength: number): string => {
+  if (!text) return "";
+  return text.length > maxLength ? text.slice(0, maxLength) + ".." : text;
+};
