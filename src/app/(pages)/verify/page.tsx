2a61a28462e396213c7fa55f0e762d810d8368c4
Ram Gollapalli <nagaramaganga@gmail.com>
2026-02-27 01:20:29 +0800

    UI changes

--- src/app/(pages)/verify/page.tsx
+++ src/app/(pages)/verify/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import signInThumb from "../../../../public/assets/img/others/sign-in-thumb.jpg";
 import VerifyForm from "@/components/Form/auth/VerifyForm";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Sign In - Property-La Real Estate React NextJs Template",
@@ -18,6 +19,11 @@ export default function Verify() {
           <div className="row justify-content-center">
             <div className="col-lg-6">
               <div className="tp-sign-in-register-box p-relative text-center">
+                <div>
+                  <Link href="/" style={{ float: "right" }}>
+                    X
+                  </Link>
+                </div>
                 <div className="tp-sign-in-register-heading mb-10">
                   <h4 className="tp-sign-in-register-title">Verify now!</h4>
                 </div>
